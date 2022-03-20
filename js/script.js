c5887dcb38967c11de27f447b41f2959bc4d8411
Nabjeet Kumar <webjit45@gmail.com>
2022-03-20 10:20:00 +0530

    Changed in UI

--- js/script.js
+++ js/script.js
@@ -1,18 +1,11 @@
-function reloadFrame(){
-  document.getElementById('vsdev').src = document.getElementById('vsdev').src;
+function startmenutoggle() {
+    const startmenu = document.getElementById('startmenu');
+    startmenu.classList.toggle('taskshow');
 }
 
-$(window).on('load', function() { // makes sure the whole site is loaded 
-  $('#status').fadeOut(); // will first fade out the loading animation 
-  $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
-  $('body').delay(350).css({'overflow':'hidden'});
-})
 
-
-var slider = document.getElementById("volcome");
-var output = document.getElementById("out");
-output.innerHTML = slider.value;
-
-slider.oninput = function() {
-  output.innerHTML = this.value;
+function volumehubtoggle() {
+    const startmenu = document.getElementById('volumehud');
+    startmenu.classList.toggle('volumeshow');
 }
+
